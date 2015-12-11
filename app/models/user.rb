e6f21615665010e1cd4bbfb53b404ba4287494a9
jimmy <jimmy@jimmytekiMacBook-Air.local>
2015-12-11 11:49:33 +0800

    login

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,10 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :authentication_keys => [:mobile]
+
+  def email_required?
+    false
+  end
 end
