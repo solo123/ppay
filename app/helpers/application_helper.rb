c3b2f3d1cd633065a047c3f2169fa4086ba37309
helapu <foolway@126.com>
2015-12-14 19:47:47 +0800

    set left menu active work

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,6 @@
 module ApplicationHelper
 
 
-  def add_table_op_menu(title)
-    render 'shared/table_op_menu',
-              :local => {:title => title}
-  end
-
   def add_left_menu_item(title, options={}, &blk)
     render 'shared/left_menu_item',
               :local => {:title => title,
