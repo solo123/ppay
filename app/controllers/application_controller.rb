13a654bce2eb81beec9aeea71a3b08fc0af54502
helapu <foolway@126.com>
2015-12-16 16:23:21 +0800

    change before action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
       end
     end
 
-  # before_action :authenticate_user!
+  before_action :authenticate_user!
 
   # Overwriting the sign_out redirect path method
   # def after_sign_out_path_for(resource_or_scope)
