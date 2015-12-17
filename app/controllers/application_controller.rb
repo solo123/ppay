6f98dce3d5972fd6c65a017704cfe52e7e8cf193
helapu <foolway@126.com>
2015-12-17 18:04:20 +0800

    auto layout mobile resoures

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,16 +11,4 @@ class ApplicationController < ActionController::Base
 
   before_action :authenticate_user!
 
-  # Overwriting the sign_out redirect path method
-  # def after_sign_out_path_for(resource_or_scope)
-  #   'http://www.baidu.com'
-  # end
-
-  # protected
-  # def configure_permitted_parameters
-  #   devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:mobile, :password, :password_confirmation, :remember_me) }
-  #   devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:mobile, :email, :password, :remember_me) }
-  #   devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:username, :email, :password, :password_confirmation, :current_password) }
-  # end
-
 end
--- app/controllers/imp_qf_clearings_controller.rb
+++ app/controllers/imp_qf_clearings_controller.rb
@@ -1,7 +1,9 @@
 class ImpQfClearingsController < ResourceController
   def initialize
     super
+    @m_fields = [1,3,5]
+
     @table_head = '清算数据'
-    @table_fields = [ '商户id', '清算日期', '交易笔数', '交易本金', '手续费', '结算金额', '实际清算金额', '状态' ]
+    @table_titles = [ '商户id', '清算日期', '交易笔数', '交易本金', '手续费', '结算金额', '实际清算金额', '状态' ]
   end
 end
