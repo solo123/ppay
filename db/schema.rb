d2550eec43120d65864dd6ad163acf43f949e7d2
helapu <foolway@126.com>
2015-12-23 19:19:45 +0800

    login errer flash messages

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151223064226) do
+ActiveRecord::Schema.define(version: 20151221032632) do
 
   create_table "addresses", force: :cascade do |t|
     t.integer  "contact_id"
