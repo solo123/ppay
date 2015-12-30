575bc234410fd580603afc6ee615bb2da8e48e0a
helapu <foolway@126.com>
2015-12-30 11:18:13 +0800

    add gem for tag

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151229044418) do
+ActiveRecord::Schema.define(version: 20151230022028) do
 
   create_table "addresses", force: :cascade do |t|
     t.integer  "addr_obj_id"
@@ -55,13 +55,12 @@ ActiveRecord::Schema.define(version: 20151229044418) do
   end
 
   create_table "clients", force: :cascade do |t|
-    t.integer  "shid"
+    t.string   "shid"
     t.string   "shop_name"
     t.string   "shop_tel"
     t.integer  "category_id"
     t.integer  "salesman_id"
     t.decimal  "rate",                    precision: 12, scale: 6
-    t.string   "join_date"
     t.decimal  "bank_card_limit_each",    precision: 12, scale: 2
     t.decimal  "bank_card_limit_month",   precision: 12, scale: 2
     t.decimal  "credit_card_limit_each",  precision: 12, scale: 2
@@ -291,6 +290,7 @@ ActiveRecord::Schema.define(version: 20151229044418) do
     t.integer  "client_id"
     t.string   "brand"
     t.string   "model"
+    t.string   "join_date"
     t.string   "info"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
@@ -311,6 +311,26 @@ ActiveRecord::Schema.define(version: 20151229044418) do
 
   add_index "salesmen_contacts", ["contact_id"], name: "index_salesmen_contacts_on_contact_id"
   add_index "salesmen_contacts", ["salesman_id"], name: "index_salesmen_contacts_on_salesman_id"
+
+  create_table "taggings", force: :cascade do |t|
+    t.integer  "tag_id"
+    t.integer  "taggable_id"
+    t.string   "taggable_type"
+    t.integer  "tagger_id"
+    t.string   "tagger_type"
+    t.string   "context",       limit: 128
+    t.datetime "created_at"
+  end
+
+  add_index "taggings", ["tag_id", "taggable_id", "taggable_type", "context", "tagger_id", "tagger_type"], name: "taggings_idx", unique: true
+  add_index "taggings", ["taggable_id", "taggable_type", "context"], name: "index_taggings_on_taggable_id_and_taggable_type_and_context"
+
+  create_table "tags", force: :cascade do |t|
+    t.string  "name"
+    t.integer "taggings_count", default: 0
+  end
+
+  add_index "tags", ["name"], name: "index_tags_on_name", unique: true
 
   create_table "trades", force: :cascade do |t|
     t.integer  "client_id"
