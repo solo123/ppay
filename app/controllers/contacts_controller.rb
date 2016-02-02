b370d3f4147dd6d7a823551d26e609dfe0229459
wxl <wxl@pooul.cn>
2016-02-02 12:13:54 +0800

    wxl

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -10,7 +10,7 @@ class ContactsController < NestedResourcesController
 
   def index
     s = params[:s]
-    @q = Contact.ransack({'name_cont': s,'tel_cont': s,'qq_cont': s,'roles_cont': s,'name_cont': s, 'm': 'or'})
+    @q = Contact.ransack({'name_cont'=> s,'tel_cont'=> s,'qq_cont'=> s,'roles_cont'=> s,'name_cont'=> s, 'm'=> 'or'})
     @collection = @q.result.page( params[:page]).per(100)
   end
 
